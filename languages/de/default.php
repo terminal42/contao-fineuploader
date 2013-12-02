6a6b410179a4dba37c99addb84f13d06e51059ae
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-12-02 13:23:46 +0100

    Added german labels

---
+++ languages/de/default.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ * PHP version 5
+ * @copyright  terminal42 gmbh 2009-2013
+ * @author     Andreas Schempp <andreas.schempp@terminal42.ch>
+ * @author     Kamil Kuźmiński <kamil.kuzminski@codefog.pl>
+ * @license    LGPL
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC']['fineuploader_drop'] = 'Datei zum hochladen hierhin ziehen';
+$GLOBALS['TL_LANG']['MSC']['fineuploader_upload'] = 'Datei hochladen';
+$GLOBALS['TL_LANG']['MSC']['fineuploader_processing'] = 'Dateien werden hochgeladen...';
