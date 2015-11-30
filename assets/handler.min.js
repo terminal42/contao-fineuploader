17a46c39e425c7e0390f2ec337365350f0888b3d
Yanick Witschi <yanick.witschi@terminal42.ch>
2015-11-30 13:19:36 +0100

    Adjusted copyrights

--- assets/handler.min.js
+++ assets/handler.min.js
@@ -1,7 +1,7 @@
 /**
  * fineuploader extension for Contao Open Source CMS
  *
- * @copyright  Copyright (c) 2008-2014, terminal42 gmbh
+ * @copyright  Copyright (c) 2008-2015, terminal42 gmbh
  * @author     terminal42 gmbh <info@terminal42.ch>
  * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
  * @link       http://github.com/terminal42/contao-fineuploader
