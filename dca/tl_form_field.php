a2129b4c8e9f2bcb2865ba1785a931bdbd00c5b3
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2016-03-18 11:59:59 +0100

    Added the "addToDbafs" option in form generator (#17)

--- dca/tl_form_field.php
+++ dca/tl_form_field.php
@@ -12,7 +12,7 @@
 /**
  * Add palettes to tl_form_field
  */
-$GLOBALS['TL_DCA']['tl_form_field']['palettes']['fineUploader'] = '{type_legend},type,name,label;{fconfig_legend},mandatory,maxConnections,extensions,maxlength,chunking,multiple;{store_legend:hide},storeFile;{expert_legend:hide},class,accesskey,tabindex,fSize';
+$GLOBALS['TL_DCA']['tl_form_field']['palettes']['fineUploader'] = '{type_legend},type,name,label;{fconfig_legend},mandatory,maxConnections,extensions,maxlength,chunking,multiple;{store_legend:hide},storeFile,addToDbafs;{expert_legend:hide},class,accesskey,tabindex,fSize';
 $GLOBALS['TL_DCA']['tl_form_field']['palettes']['__selector__'][] = 'chunking';
 $GLOBALS['TL_DCA']['tl_form_field']['subpalettes']['chunking'] = 'chunkSize,concurrent';
 
@@ -54,5 +54,14 @@ $GLOBALS['TL_DCA']['tl_form_field']['fields']['concurrent'] = array
     'exclude'                 => true,
     'inputType'               => 'checkbox',
     'eval'                    => array('tl_class'=>'w50 m12'),
+    'sql'                     => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_form_field']['fields']['addToDbafs'] = array
+(
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['addToDbafs'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class'=>'clr'),
     'sql'                     => "char(1) NOT NULL default ''"
 );
