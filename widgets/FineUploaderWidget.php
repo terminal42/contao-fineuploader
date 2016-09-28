34369511754059851bc086d94c3634c0411f4484
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2016-09-28 16:00:52 +0200

    Fix the CSS issues in Contao 4

--- widgets/FineUploaderWidget.php
+++ widgets/FineUploaderWidget.php
@@ -74,6 +74,10 @@ class FineUploaderWidget extends FineUploaderBase
         $GLOBALS['TL_JAVASCRIPT']['fineuploader']         = 'system/modules/fineuploader/assets/fine-uploader/fine-uploader.min.js';
         $GLOBALS['TL_JAVASCRIPT']['fineuploader_handler'] = 'system/modules/fineuploader/assets/handler.min.js';
         $GLOBALS['TL_CSS']['fineuploader_handler']        = 'system/modules/fineuploader/assets/handler.min.css';
+
+        if (class_exists('Contao\CoreBundle\Framework\ContaoFrameworkInterface')) {
+            $GLOBALS['TL_CSS']['fineuploader_contao4'] = 'system/modules/fineuploader/assets/contao4.css';
+        }
     }
 
     /**
