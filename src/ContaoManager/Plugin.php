21781047118fdbaa5271d68a4457cf0482aa81ee
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2017-08-02 11:01:02 +0200

    Make the bundle compatible with Contao 4.4

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Terminal42\FineUploaderBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Terminal42\FineUploaderBundle\Terminal42FineUploaderBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(Terminal42FineUploaderBundle::class)->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
