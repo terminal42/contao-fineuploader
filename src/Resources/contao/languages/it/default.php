b2b6aed3f374a9efcbd7482aa908292cbbacf8da
PaoloB <paolob@contaocms.it>
2020-05-31 00:47:42 +0200

    Italian Language
    Italian translation of the extension

---
+++ src/Resources/contao/languages/it/default.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * FineUploader Bundle for Contao Open Source CMS.
+ *
+ * @copyright  Copyright (c) 2020, terminal42 gmbh
+ * @author     terminal42 <https://terminal42.ch>
+ * @license    MIT
+ */
+
+$GLOBALS['TL_LANG']['MSC']['fineuploader.error'] = 'Errore sconosciuto';
+$GLOBALS['TL_LANG']['MSC']['fineuploader.drop'] = 'Trascina qui i file per caricarli';
+$GLOBALS['TL_LANG']['MSC']['fineuploader.upload'] = 'Carica qui il file';
+$GLOBALS['TL_LANG']['MSC']['fineuploader.processing'] = 'Elaborazione dei file…';
+$GLOBALS['TL_LANG']['MSC']['fineuploader.cancel'] = 'Annulla';
+$GLOBALS['TL_LANG']['MSC']['fineuploader.retry'] = 'Riprova';
+$GLOBALS['TL_LANG']['MSC']['fineuploader.delete'] = 'Elimina';
+$GLOBALS['TL_LANG']['MSC']['fineuploader.close'] = 'Chiudi';
+$GLOBALS['TL_LANG']['MSC']['fineuploader.yes'] = 'Si';
+$GLOBALS['TL_LANG']['MSC']['fineuploader.no'] = 'No';
