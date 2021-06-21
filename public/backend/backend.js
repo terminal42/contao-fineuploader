67cefbeba191fc4d36aeb6006f2efde3582294f6
Andreas Schempp <andreas.schempp@terminal42.ch>
2021-06-21 16:40:00 +0200

    Updated Node dependencies

--- public/backend/backend.js
+++ public/backend/backend.js
@@ -1,11 +1,3 @@
-/**
- * fineuploader extension for Contao Open Source CMS
- *
- * @copyright Copyright (c) 2008-2015, terminal42 gmbh
- * @author    terminal42 gmbh <info@terminal42.ch>
- * @license   http://opensource.org/licenses/lgpl-3.0.html LGPL
- * @link      http://github.com/terminal42/contao-fineuploader
- */
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define([], factory(root));
