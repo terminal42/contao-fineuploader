b38fe8b2df44bb89aa2a8cab9a534858710eb359
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2022-05-16 15:43:33 +0200

    Fix a potential PHP 8 warning

--- src/ConfigGenerator.php
+++ src/ConfigGenerator.php
@@ -67,7 +67,7 @@ class ConfigGenerator
             'minSizeLimit' => $config->getMinSizeLimit(),
             'sizeLimit' => $config->getMaxSizeLimit(),
             'uploadButtonTitle' => $config->getUploadButtonTitle(),
-            'messages' => $config->getLabels()['messages'],
+            'messages' => $config->getLabels()['messages'] ?? [],
         ];
 
         // Enable the chunking
